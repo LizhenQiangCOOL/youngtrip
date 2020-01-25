127011db4ecee66c8d5200e7f6103a30c1de21ff
Lizhenqiang <lzq@cincore.com>
2020-01-25 22:32:16 +0800

    change router hash && make githubpage

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,9 @@ module.exports = {
     "vuetify"
   ],
 
-  publicPath:"/", //根路径
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/youngtrip/'
+    : '/',   //根路径
   outputDir:"dist", //构建输出目录
   assetsDir:"assets", //静态资源目录　（js, css, img, fonts）
   lintOnSave:false, //是否开启estlint保存检查
