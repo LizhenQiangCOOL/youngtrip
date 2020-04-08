72dad46876d0becca41d1914d680ce791e0cccd2
Lizhenqiang <lzq@cincore.com>
2020-04-08 18:43:41 +0800

    npm run build

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import axios from 'axios'
 import './filters'
 
 // axios全局设置
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
-// axios.defaults.baseURL = 'https://xn--m83a.top/api'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
+axios.defaults.baseURL = 'https://xn--m83a.top/api'
 // axios.defaults.baseURL = process.env.VUE_APP_APIURL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
